c2b0f8fcc4a302e3bc89faa38cd655e98db8b85a
Bill Thornton <bill@exclamationlabs.com>
2014-08-07 14:23:17 -0400

    Add attribution for docker-rm-all and docker-rmi-all

--- docker-rm-all.sh
+++ docker-rm-all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # This script removes ALL docker containers
+# Originally found at http://jimhoskins.com/2013/07/27/remove-untagged-docker-images.html
 
 docker rm $(docker ps -a -q)
--- docker-rmi-all.sh
+++ docker-rmi-all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # This script removes ALL docker images
+# Originally found at http://jimhoskins.com/2013/07/27/remove-untagged-docker-images.html
 
 docker rmi $(docker images -q)
